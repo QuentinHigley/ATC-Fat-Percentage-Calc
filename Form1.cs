e00763b4ad0963c1e13ba95eaadd442f2790a1bc
Quentin Higley <QuentinHigley@gmail.com>
2017-07-13 13:27:04 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FatPercentageCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            Double dblCal, dblGram, dblTotal, dblPercentage;
+            dblCal = Convert.ToDouble(txtCal.Text);
+            dblGram = Convert.ToDouble(txtGrams.Text);
+            if(dblCal < 0 || dblGram < 0 || dblGram > dblCal)
+            { MessageBox.Show("Incorrect input of data"); }
+            else { 
+            dblTotal = dblGram * 9;
+            dblPercentage = dblTotal / dblCal;
+            if (chkWantToKnow.Checked == true && dblPercentage < 0.3)
+            {
+                
+                lblOut.Text = "Total Number of Calories From fat: "+ dblTotal.ToString("n2") + Environment.NewLine + 
+                    "Percentage of Calories that come from fat: " + dblPercentage.ToString("P") + Environment.NewLine + 
+                    "Low Fat food";
+
+            }
+            else
+            {
+               
+                lblOut.Text = "Total Number of Calories From fat: " + dblTotal.ToString("n2") + Environment.NewLine +
+                    "Percentage of Calories that come from fat: " + dblPercentage.ToString("P");
+            }
+            }
+        }
+    }
+}
